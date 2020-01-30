bd8a8f1de86b3e62c5da7a330127673075a06506
palak <palakmpatel888@gmail.com>
2020-01-29 23:08:58 -0500

    testing first try

---
+++ app/src/test/java/com/gohool/firstlook/tipcalculator/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.gohool.firstlook.tipcalculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    @Test
+    void test1(){
+        assertEquals(2+2,4);
+    }
+
+
+}
